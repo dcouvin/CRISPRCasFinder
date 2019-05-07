a20298a536e779481aa78270a336eaaa5a4a03c4
David Couvin <david.couvin@googlemail.com>
2019-05-07 10:59:58 -0400

    Update installer_UBUNTU.sh

--- installer_UBUNTU.sh
+++ installer_UBUNTU.sh
@@ -2,7 +2,7 @@
 
 # shell script allowing to install all CRISPRCasFinder.pl-v4.2's dependencies
 #
-# same version than CRISPRCasFinder.pl, here 4.2.17
+# same version than CRISPRCasFinder.pl, here 4.2.19
 # authors: David Couvin, Fabrice Leclerc, Claire Toffano-Nioche
 
 #------------------------
